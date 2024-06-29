ea03afab40cd4eaafd015a8c434e0fb5862ba2a2
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-06-29 09:08:46 +0530

    Update and make it a book store

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -1,20 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract DSA{
-    function reQuire(uint questions) public pure {
-        require(questions > 200, "Sorry, you need to practice more!");
+contract Bookstore {
+    address public owner;
+    uint public types;
+    uint public bookCount;
+    struct Book { uint price; uint stock; }
+    mapping(uint => Book) public books;
+    uint public storeMoney;
+    uint purchased=0;
+
+    constructor() {
+        owner = msg.sender;
+        storeMoney = 10;
     }
 
-    function reVert(uint questions) public pure {
-        if (questions <= 200) {
-            revert("Sorry, you need to practice more!");
-        }
+    function addBook(uint stock, uint price) public payable{
+        require(msg.sender == owner, "Not owner");
+        require(price > 0 && stock > 0, "Invalid input");
+        require(storeMoney >= price*stock);
+        books[++types] = Book(price, stock);
+        bookCount += stock;
+        storeMoney -= price*stock;
     }
 
-    uint public num=201;
+    function mintMoney(uint value) public{
+        storeMoney += value;
+    }
 
-    function testAssert() public view {
-        assert(num > 200);
+    function purchaseBook(uint id) public payable {
+        Book storage book = books[id];
+        require(book.price > 0 && book.stock > 0, "Invalid purchase");
+        book.stock--;
+        assert(book.stock >= 0);
+        storeMoney += books[id].price;
+        bookCount--;
+        purchased++;
+    }
+
+    function refund(uint id) public{
+        require(msg.sender == owner, "Not owner");
+        assert(storeMoney>=books[id].price);
+        books[id].stock++;
+        bookCount++;
+        assert(purchased>=0);
+        purchased--;
     }
 }
